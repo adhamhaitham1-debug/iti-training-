bf763f0dea54984e3536f334d03aa0e69ad147cd
Your Name <your-email@example.com>
2026-08-11 18:06:04 +0300

    Add Task 3 Assignment 1 arithmetic

---
+++ linux/Task_3/Assignment_1/arithmetic.sh
@@ -0,0 +1,8 @@
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+echo ""
+echo "Sum = $((num1 + num2))"
+echo "Difference = $((num1 - num2))"
+echo "Multiplication = $((num1 * num2))"
+echo "Division = $((num1 / num2))"
+echo "Modulus = $((num1 % num2))"
